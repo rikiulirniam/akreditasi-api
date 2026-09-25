de4b84b62196cffb9cc68625ad30bf12f463a99e
rikiulirniam <rikiulir@gmail.com>
2026-09-25 14:28:12 +0700

    chore: add hi route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response()->json(["message" => "Welcome to API Akreditasi"], 200);
 });
+
+Route::get('/', function () {
+    return response()->json(["message" => "Welcome to API Akreditasi"], 200);
+});
+Route::get('/hi', function () {
+    return response()->json(["message" => "Hai"], 200);
+});
